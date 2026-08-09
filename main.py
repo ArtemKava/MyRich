a405cb52c1966389ee990d21a2d560107aea75bf
ArtemKava <artem27igor@gmail.com>
2026-08-09 15:00:26 +0300

    test commit

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ class App:
                               "for now this a all information for base test")
         interface.give_signal("message", "DONE")
 if __name__ == '__main__':
+    print("Hello World")
     app = App()
